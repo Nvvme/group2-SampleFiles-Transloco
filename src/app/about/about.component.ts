7d84e3e48a83407c739107e656b467aced8379d3
Nvvme <Navan2400@gmail.com>
2025-04-05 23:15:52 -0400

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+@Component({
+  selector: 'app-about',
+  imports: [TranslocoDirective],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+@Component({
+  selector: 'app-home',
+  imports: [TranslocoDirective],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
