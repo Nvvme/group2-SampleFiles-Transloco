508927583c10c4e792d3e75310841adbaa42534a
Nvvme <Navan2400@gmail.com>
2025-04-06 00:26:04 -0400

    Done with steps 4 and 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
-import { Routes } from '@angular/router';
-import { provideTranslocoScope } from '@jsverse/transloco';
-
-export const routes: Routes = [
+/* Step 5c: Routing Configuration for the About Component
+   // This routing configuration lazy loads the About component and its 'about' translation scope.
+   // When activated, navigating to '/about' will load the About component with the appropriate translations.
+   
+export const routes = [
   {
     path: '',
-    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
   },
   {
     path: 'about',
-    loadComponent: () => import('./about/about.component').then((m) => m.AboutComponent),
-    providers: [provideTranslocoScope('about')] // lazy load 'about' scope
+    loadComponent: () => import('./about/about.component').then(m => m.AboutComponent),
+    providers: [provideTranslocoScope('about')]
   },
   {
     path: '**', redirectTo: '/'
   }
 ];
+*/
